535e54645cd439a38db01807e4fa90d7936dc3b2
FallenMax <FallenMax@gmail.com>
2020-09-17 17:16:48 +0800

    use wrap-output as purge-input

--- src/cli/purge.test.tsx
+++ src/cli/purge.test.tsx
@@ -5,7 +5,7 @@ import { purgers } from './purge'
 describe('purge', () => {
   test('remove a18n() calls and imports ', () => {
     const source = readFileSync(
-      resolve(__dirname, '../../src/cli/__test__/purge-input.mock.tsx'),
+      resolve(__dirname, '../../src/cli/__test__/wrap-output.mock.tsx'),
       { encoding: 'utf-8' },
     )
     const expected = readFileSync(
@@ -13,6 +13,21 @@ describe('purge', () => {
       { encoding: 'utf-8' },
     )
 
+    expect(purgers.tsx(source, {})).toBe(expected)
+    // ensure nothing changes for second purge
+    expect(purgers.tsx(expected, {})).toBe(expected)
+  })
+
+  test('remove a18n methods', () => {
+    const source = `const { getA18n } = require('a18n')
+const a18n__ = getA18n('your-namespace')
+
+/* @a18n-ignore-file */
+// @a18n-ignore
+const a18n_ = require('a18n')
+a18n.addLocaleResource('en-US', {})
+a18n.setLocaleSync('zh-CN')`
+    const expected = ``
     expect(purgers.tsx(source, {})).toBe(expected)
     // ensure nothing changes for second purge
     expect(purgers.tsx(expected, {})).toBe(expected)
